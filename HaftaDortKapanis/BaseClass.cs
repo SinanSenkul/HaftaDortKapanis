7a75021bbf4582827500c86068fdde9681f36b24
sinan <sinan@SINAN-SENKUL>
2024-08-15 07:50:01 +0300

    Add project files.

---
+++ HaftaDortKapanis/BaseClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public abstract class BaseMachine //base class created
+{
+    public DateTime ProdDate { get; set; }
+    public int SerialNum { get; set; }
+    public string Name { get; set; }
+    public string Detail { get; set; }
+    public string OperatingSystem { get; set; }
+    public BaseMachine(DateTime prodDate, int serialNum, string name, string detail, string operatingSystem)
+    {
+        ProdDate = DateTime.Now;
+        SerialNum = serialNum;
+        Name = name;
+        Detail = detail;
+        OperatingSystem = operatingSystem;
+    }
+    public virtual void LogInfo()
+    {
+        Console.WriteLine($"prod date: {ProdDate}, serial num: {SerialNum}, details: {Detail}, opr sys: {OperatingSystem}");
+    }
+    public abstract void GetProductName(); //UrunAdiGetir()
+}
+
+public class Telephone : BaseMachine //Telephone class uses BaseMachine class
+{
+    public bool IsTRLicensed { get; set; }
+    public Telephone(DateTime prodDate, int serialNum, string name, string detail,
+        string operatingSystem, bool isTRLicensed) : base(prodDate, serialNum, name, detail, operatingSystem)
+    {
+        IsTRLicensed = isTRLicensed;
+    }
+    public override void LogInfo() //overridden LogInfo method
+    {
+        Console.WriteLine($"prod date: {ProdDate}, serial num: {SerialNum}, details: {Detail}, opr sys: {OperatingSystem}, " +
+            $"TR licence: {IsTRLicensed}");
+    }
+
+    public override void GetProductName() //abstract method being implemented for Telephone class
+    {
+        Console.WriteLine($"Telefonunuzun adı: {Name}");
+    }
+}
+
+public class Computer : BaseMachine
+{
+    public int USBQty { get; set; }
+    public bool HasBluetooth { get; set; }
+    public Computer(DateTime prodDate, int serialNum, string name, string detail, string operatingSystem,
+        int usbQty, bool hasBluetooth) : base(prodDate, serialNum, name, detail, operatingSystem)
+    {
+        if (usbQty != 2 || usbQty != 4)
+        {
+            Console.WriteLine("USB giriş sayısı 2 ya da 4 olabilir...");
+            USBQty = -1;
+        }
+        else
+        {
+            USBQty = usbQty;
+        }
+        HasBluetooth = hasBluetooth;
+    }
+    public override void LogInfo()
+    {
+        Console.WriteLine($"prod date: {ProdDate}, serial num: {SerialNum}, details: {Detail}, opr sys: {OperatingSystem}, " +
+            $"USB quantity: {USBQty}, has Bluetooth: {HasBluetooth}");
+    }
+    public override void GetProductName()
+    {
+        Console.WriteLine($"Bilgisayarınızın adı: {Name}");
+    }
+}
---
+++ HaftaDortKapanis/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace HaftaDortKapanis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            do
+            {
+                Console.WriteLine("Telefon üretmek için 1'e, bilgisayar üretmek için 2'ye basınız...");
+                string selection = Console.ReadLine();
+                if (selection == "1")
+                {
+                    Console.WriteLine("Telefon seri numarası:");
+                    int serialNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Telefon hakkında açıklama:");
+                    string detail = Console.ReadLine();
+                    Console.WriteLine("Telefon adı:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Telefonun işletim sistemi:");
+                    string operatingSystem = Console.ReadLine();
+                    Console.WriteLine("Telefon TR lisanslı mı? (evet için 'e', hayır için herhangi bir tuşa basın):");
+                    string trLicense = Console.ReadLine();
+                    bool isTRLicensed = false;
+                    if (trLicense == "e")
+                    {
+                        isTRLicensed = true;
+                    }
+                    DateTime prodDate = DateTime.Now;
+                    BaseMachine phone = new Telephone(prodDate, serialNumber, name, detail, operatingSystem, isTRLicensed);
+                    phone.LogInfo();
+                }
+                else if (selection == "2")
+                {
+                    Console.WriteLine("Bilgisayar seri numarası:");
+                    int serialNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Bilgisayar hakkında açıklama:");
+                    string detail = Console.ReadLine();
+                    Console.WriteLine("Bilgisayar adı:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Bilgisayar işletim sistemi:");
+                    string operatingSystem = Console.ReadLine();
+                    Console.WriteLine("Bluetooth özelliği var mı? (evet için 'e', hayır için herhangi bir tuşa basın):");
+                    string bluetooth = Console.ReadLine();
+                    bool hasBluetooth = false;
+                    if (bluetooth == "e")
+                    {
+                        hasBluetooth = true;
+                    }
+                    Console.WriteLine("Bilgisayar USB çıkış adedi (2 veya 4 olmalıdır):");
+                    int usbQty = Convert.ToInt32(Console.ReadLine());
+                    DateTime prodDate = DateTime.Now;
+                    BaseMachine computer = new Computer(prodDate, serialNumber, name, detail, operatingSystem, usbQty, hasBluetooth);
+                    computer.LogInfo();
+                }
+
+                Console.WriteLine("Ürününüz başarıyla oluşturuldu, yeni ürün oluşturmak için 'e' tuşuna basabilirsiniz " +
+                    "ya da çıkmak için herhangi başka bir tuşa basabilirsiniz");
+                string newProduct = Console.ReadLine();
+                if (newProduct == "e")
+                {
+                    loop = true;
+                }
+                else
+                {
+                    loop = false;
+                }
+            }
+            while (loop);
+            Console.WriteLine("Hoşçakalın!");
+        }
+    }
+}
